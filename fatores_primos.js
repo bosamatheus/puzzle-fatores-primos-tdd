2b879ca1d9f91695dbf9f11fbad3b832f7a19756
Bosa <mfbs@gft.com>
2020-02-13 16:26:20 -0300

    Implementa testes sobre as entradas de exemplo

--- fatores_primos.js
+++ fatores_primos.js
@@ -1,5 +1,51 @@
 const encontraFatoresPrimos = (numero) => {
-    return numero
+
+    if (numero == 0 || numero == 1) {
+        return numero + " = " + numero
+    }
+
+    let fatores = []
+    let dividendo = numero
+    let divisor = 2
+
+    while (dividendo != 1) {
+
+        if (dividendo % divisor == 0) {
+            fatores.push(divisor)
+            dividendo = dividendo / divisor
+        } else {
+            divisor = proximoPrimo(divisor)
+        }
+    }
+
+    resposta = numero.toString() + " = " + fatores.join(" x ")
+
+    return resposta
+}
+
+function proximoPrimo(atual) {
+    let proximo = atual + 1
+    
+    while (!verificaPrimo(proximo)) {
+        proximo++
+    }
+
+    return proximo
+}
+
+function verificaPrimo(numero) {
+    let contador = 0
+    
+    for (let divisor = 1; divisor < numero; divisor++) {
+        if (numero % divisor == 0) {
+            contador++
+            if (contador > 1) {
+                return false
+            }
+        }
+    }
+
+    return true
 }
 
 module.exports = {
--- fatores_primos.test.js
+++ fatores_primos.test.js
@@ -1,6 +1,33 @@
 const { encontraFatoresPrimos } = require('./fatores_primos')
 
-test('Testa se um número primo retorna ele mesmo', () => {
-    const primo = 5
-    expect(encontraFatoresPrimos(primo)).toEqual(primo)
+test("Testa se 0 retorna ele mesmo", () => {
+    expect(encontraFatoresPrimos(0)).toEqual("0 = 0")
+})
+
+test("Testa se 1 retorna ele mesmo", () => {
+    expect(encontraFatoresPrimos(1)).toEqual("1 = 1")
+})
+
+test("Testa se o número primo 5 retorna ele mesmo", () => {
+    expect(encontraFatoresPrimos(5)).toEqual("5 = 5")
+})
+
+test("Testa se o número primo 997 retorna ele mesmo", () => {
+    expect(encontraFatoresPrimos(997)).toEqual("997 = 997")
+})
+
+test("Testa se um número não primo qualquer retorna seus fatores", () => {
+    expect(encontraFatoresPrimos(4)).toEqual("4 = 2 x 2")
+})
+
+test("Testa se retorna os fatores primos de 100", () => {
+    expect(encontraFatoresPrimos(100)).toEqual("100 = 2 x 2 x 5 x 5")
+})
+
+test("Testa se retorna os fatores primos de 198", () => {
+    expect(encontraFatoresPrimos(198)).toEqual("198 = 2 x 3 x 3 x 11")
+})
+
+test("Testa se retorna os fatores primos de 276", () => {
+    expect(encontraFatoresPrimos(276)).toEqual("276 = 2 x 2 x 3 x 23")
 })
